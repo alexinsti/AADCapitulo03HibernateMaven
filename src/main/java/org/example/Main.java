db0d79b6d0f37ea53c27588c1f6749763176f093
Alejandro <aalvarez55@ieszaidinvergeles.org>
2023-01-09 11:17:58 +0100

    hasta el errorCode0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final StandardServiceRegistry registro = new StandardServiceRegistryBuilder().configure().build();
+        final SessionFactory factory = new MetadataSources(registro).buildMetadata().buildSessionFactory();
+        Session session = factory.openSession();
     }
 }
